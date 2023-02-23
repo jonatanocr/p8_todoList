7e230c59fcc8ccbeec68ad649fe392a38f45387f
jonatanocr <jmbzk@pm.me>
2023-02-23 05:33:11 +0100

    add delete user logic

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,6 +10,7 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
@@ -69,5 +70,19 @@ class UserController extends AbstractController
         return $this->render('user/update.html.twig', [
             'form' => $form,
         ]);
+    }
+
+    #[Route('/user/delete', name: 'delete_user')]
+    public function delete(Request $request, ManagerRegistry $doctrine, Session $session): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $user = $this->getUser();
+
+        $session = new Session();
+        $session->invalidate();
+        $entityManager->remove($user);
+        $entityManager->flush();
+        return $this->redirectToRoute('app_login');
+
     }
 }
