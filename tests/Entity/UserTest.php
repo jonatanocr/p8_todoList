2565c33027960fbdb414e6533ffba987d7b970ee
jonatanocr <jmbzk@pm.me>
2023-03-21 04:34:18 +0100

    Add test for Task entity

---
+++ tests/Entity/UserTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use \DateTimeImmutable;
+use App\Entity\Task;
+use App\Entity\User;
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+
+}
